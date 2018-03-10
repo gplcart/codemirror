47ed2fda3adb30165a1b3fb5d44639c6e46d3ab9
GplCart <gplcart.software@gmail.com>
2018-03-10 22:37:04 +0200

    Removed core Library::clearCache()

--- Main.php
+++ Main.php
@@ -79,38 +79,6 @@ class Main
     }
 
     /**
-     * Implements hook "module.enable.after"
-     */
-    public function hookModuleEnableAfter()
-    {
-        $this->library->clearCache();
-    }
-
-    /**
-     * Implements hook "module.disable.after"
-     */
-    public function hookModuleDisableAfter()
-    {
-        $this->library->clearCache();
-    }
-
-    /**
-     * Implements hook "module.install.after"
-     */
-    public function hookModuleInstallAfter()
-    {
-        $this->library->clearCache();
-    }
-
-    /**
-     * Implements hook "module.uninstall.after"
-     */
-    public function hookModuleUninstallAfter()
-    {
-        $this->library->clearCache();
-    }
-
-    /**
      * Add CodeMirror library and extra files
      * @param \gplcart\core\Controller $controller
      * @throws InvalidArgumentException
